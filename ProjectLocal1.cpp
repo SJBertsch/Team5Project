2f55748c01eb3f988cf077659e6715be23343648
SJBertsch <sebastian.bertsch@gmail.com>
2013-11-14 14:38:25 -0800

    Made first change to project local1

--- ProjectLocal1.cpp
+++ ProjectLocal1.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Test of updating project" << endl; // prints !!!Hello World!!!
 	return 0;
 }
