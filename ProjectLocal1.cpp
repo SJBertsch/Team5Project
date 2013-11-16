c08ddc3cd4ec62f9468c07e0dd6238522bcbdfe0
SJBertsch <sebastian.bertsch@gmail.com>
2013-11-15 19:47:27 -0800

    demoing git in eclipse

--- ProjectLocal1.cpp
+++ ProjectLocal1.cpp
@@ -14,6 +14,7 @@ using namespace std;
 int main() {
 	cout << "updated a second time" << endl;
 	cout << "update a third time" << endl ;
+	cout << "update after dillons magic" << endl ;
 	tester1() ;
 	return 0 ;
 }
