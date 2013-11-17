c9632243c368bd5557a0fdc068e9cd8a9457d2a5
paul hayter <phayter1@gmail.com>
2013-11-17 14:09:43 -0800

    Broke out cpp for Record and Contacts Class

--- Case_Compile2.cpp
+++ Case_Compile2.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 #include "record.h"
 #include "contacts.h"
+#include "record.cpp"
+#include "contacts.cpp"
 
 int main(){ //PH
 // Tests classes Record and Contacts
@@ -39,74 +41,6 @@ int main(){ //PH
 	return 0;
 }
 
-//
-// function implementations  PH
-//
-int Record::inputRecord(Record person){
-/* fills Record with info from the user and returns a status code */
-   cout << "Arrived at inputRecord \n";
-   return 98;
-}
-
-void Record::printRecord(string name){
-/* prints Record with last name 'name' */
-   cout << "Arrived at printRecord \n";
-}
-
-Contacts::Contacts(){
-/* initializes private variables */
-   recordCounter = 99;  //Application should initialize to 0
-}
-
-int Contacts::getRecordCounter(){
-/* return value of recordCounter */
-   cout << "Arrived at getRecordCounter \n";
-   return recordCounter;
-}
-void Contacts::setRecordCounter(int number){ // PH
-/* sets value of recordCounter to number */
-   cout << "Arrived at setRecordCounter \n";
-   recordCounter = number;
-}
-
-void Contacts::printContacts(){
-/* prints all Record objects in class in alphabetized order */
-   cout << "Arrived at printContacts \n";  
-   int status = person[0].inputRecord(person[0]);
-   cout << "Status from inputRecord is (expect 98): " << status << "\n\n";
-   person[0].printRecord("Bart");
-}
-
-int Contacts::readContacts(){
-/* sets all Record objects in class to random ZIP and phone values. 
-Returns a status code.*/
-   cout << "Arrived at readContacts \n";
-   return 97;
-}
-
-int Contacts::closeContacts(){
-/* writes all Record objects in class to disk. Returns a status code.*/
-   cout << "Arrived at closeContacts \n";
-   return 96;
-}
-
-int Contacts::enterNew(){
-/* enters new Record object into array and returns status code. */
-   cout << "Arrived at enterNew \n";
-   return 95;
-}
-
-int Contacts::findPrintRecord(){
-/* finds and prints Record(s) and returns status code */
-   cout << "Arrived at findPrintRecord \n";
-   return 94;
-}
-
-void Contacts::swapRecords(int first, int second){
-/* swaps position of Records in the Contacts list (used for alphabetizing list */
-   cout << "Arrived at swapRecords \n";   
-}
-
 //////////////////////// END OF MODULE ////////////////////////////////////////
 
 /************************************ OUTPUT *********************************
@@ -135,4 +69,3 @@ Exiting QualitySoft Application with status (expect 96): 96
 
 Press any key to continue . . .
 ******************************************************************************/
-
---
+++ record.cpp
@@ -0,0 +1,14 @@
+//
+// Methods for Class Record // PH
+//
+
+int Record::inputRecord(Record person){
+/* fills Record with info from the user and returns a status code */
+   cout << "Arrived at inputRecord \n";
+   return 98;
+}
+
+void Record::printRecord(string name){
+/* prints Record with last name 'name' */
+   cout << "Arrived at printRecord \n";
+}
