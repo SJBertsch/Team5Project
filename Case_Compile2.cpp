115888c77eda38b77ba38b69c28f028d4b8428a9
Derrickhho <derrickhho@gmail.com>
2013-11-21 19:11:44 -0800

    Revert e865e9d..b8acbdd
    This rolls back to commit e865e9d34026ee038109a37bc16c56e0dce06005.

---
+++ Case_Compile2.cpp
@@ -0,0 +1,72 @@
+// Case_compile2.cpp : Defines the entry point for the console application.
+//
+// Module contains test main() for classes Record and Contacts to demonstrate
+// that all methods can be called and will response.
+
+//testing revision from dillon
+//testing revision from sebastian
+//testing revision from derrick
+#include <iostream>
+#include "record.h"
+#include "contacts.h"
+
+using namespace std;
+
+int main(){ //PH
+// Tests classes Record and Contacts
+   int status=0;
+
+   cout << "Testing classes Record and Contacts \n\n";
+   Contacts list; //constructs Contacts list object including memory allocation
+   
+   cout << "Constructor called and setRecordCounter called (expect 99): " 
+      << list.getRecordCounter() << "\n\n";
+
+   status = list.readContacts(); // reads Contacts list
+   cout << "readContacts returns status code (expect 97): " << status << "\n\n";
+
+   list.printContacts();
+
+   status = list.enterNew();
+   cout << "enterNew returns status code (expect 95): " << status << "\n\n";
+
+   status = list.findPrintRecord();
+   cout << "enterNew returns status code (expect 94): " << status << "\n\n";
+
+   list.swapRecords(1,2);
+ 
+   status = list.closeContacts();
+   cout << "Exiting QualitySoft Application with status (expect 96): " 
+      << status << "\n\n";
+
+	return 0;
+}
+
+//////////////////////// END OF MODULE ////////////////////////////////////////
+
+/************************************ OUTPUT *********************************
+Testing classes Record and Contacts
+
+Arrived at getRecordCounter
+Constructor called and setRecordCounter called (expect 99): 99
+
+Arrived at readContacts
+readContacts returns status code (expect 97): 97
+
+Arrived at printContacts
+Arrived at inputRecord
+Status from inputRecord is (expect 98): 98
+
+Arrived at printRecord
+Arrived at enterNew
+enterNew returns status code (expect 95): 95
+
+Arrived at findPrintRecord
+enterNew returns status code (expect 94): 94
+
+Arrived at swapRecords
+Arrived at closeContacts
+Exiting QualitySoft Application with status (expect 96): 96
+
+Press any key to continue . . .
+******************************************************************************/
